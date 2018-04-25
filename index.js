3b079dc55cf4a01398910fd554c849748f957cbc
glandon22 <glandonwright@gmail.com>
2018-04-25 06:20:02 -0400

    initing stuff

---
+++ index.js
@@ -0,0 +1,14 @@
+/*
+var express = require('express');
+
+var app = express();
+
+app.get('/', function(req,res) {
+    console.log('hello');
+});
+
+app.listen(8080);
+
+*/
+
+console.log('fghdfggfddgf');
