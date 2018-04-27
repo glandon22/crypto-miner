4c6dce96c915808fbf4154f6656f2ea25c5be134
glandon22 <glandonwright@gmail.com>
2018-04-27 08:09:30 -0400

    added all coin data to AWS for date ended 4/26/18

--- index.js
+++ index.js
@@ -3,27 +3,20 @@ var mysql = require('mysql');
 
 //skip file paths
 for (var i = 2; i < process.argv.length; i++) {
-    request.get('https://coinmetrics.io/data/' + process.argv[i] + '.csv', function (error, response, body) {
-        var date = [];
-        var tradeVol = [];
-        var tradeCount = [];
-        var marketCap = [];
-        var price = [];
-        var exchangeVol = [];
-        var genCoins = [];
-        var fees = [];
-        var fuck = [];
-
+    var coinName = process.argv[i]
+    request.get('https://coinmetrics.io/data/' + coinName + '.csv', function (error, response, body) {
         if (!error && response.statusCode == 200) {
             var csv = body.split(',');
             csv[8] = csv[8].split('\n');
             //flatten array
             var merged = [].concat.apply([], csv);
-            //console.log(merged);
             //skip column names
+            var rowHolder = [];
+            var fullCoinData = [];
             for (var j = 9; j < csv.length; j++) {
+                /*
                 if (j % 9 === 0 ) {
-                    //first date entry doesnt havfe line break character
+                    //first date entry doesnt have line break character
                     if (j === 9) { 
                         date.push(merged[j]);
                         continue;
@@ -32,40 +25,34 @@ for (var i = 2; i < process.argv.length; i++) {
                     //remove leading line break
                     date.push(merged[j].split('\n')[1]);
                 }
+                */
+                //clean date of leading new line character
+                if (j % 9 === 0) {
+                    //first date entry doesnt have line break character
+                    if (j === 9) {
+                        //add coin abbreviation as name
+                        rowHolder.push(coinName.toString()); 
+                        rowHolder.push(merged[j]);
+                        continue;
+                    }
 
-                else if ( j % 9 === 1) {
-                    tradeVol.push(merged[j]);
+                    rowHolder.push(coinName.toString()); 
+                    //remove leading line break
+                    rowHolder.push(merged[j].split('\n')[1]);
                 }
-
-                else if ( j % 9 === 2) {
-                    tradeCount.push(merged[j]); 
+                //push row into table holder
+                else if (j % 9 === 8) {
+                    fullCoinData.push(rowHolder);
+                    rowHolder = [];
                 }
-
-                else if ( j % 9 === 3) {
-                    marketCap.push(merged[j]);
+                //push value into row holder
+                else {
+                    rowHolder.push(merged[j]);
                 }
-
-                else if ( j % 9 === 4) {
-                    price.push(merged[j]);
-                }
-
-                else if ( j % 9 === 5) {
-                    exchangeVol.push(merged[j]);
-                }
-
-                else if ( j % 9 === 6) {
-                    genCoins.push(merged[j]);
-                }
-
-                else if ( j % 9 === 7) {
-                    fees.push(merged[j]);
-                }
-                //throw away null values
-                else if ( j % 9 === 8) {
-                }
-            } 
+            }
+            console.log(fullCoinData); 
         }
-
+        
         var con = mysql.createConnection({
             host: "cryptos.cvndjrqk9gtt.us-east-2.rds.amazonaws.com",
             user: "",
@@ -76,20 +63,24 @@ for (var i = 2; i < process.argv.length; i++) {
           con.connect(function(err) {
             if (err) throw err;
             console.log("Connected!");
-          
-            /* creating table
+          /*
+            creating table
             var sql = "CREATE TABLE coins (date DATE, tradeVol INT, tradeCount INT, marketCap BIGINT, price DOUBLE(10,2), exchangeVol INT, genCoins INT, fees INT)";
             con.query(sql, function(err,results) {
               if (err) throw err;
               console.log('created');
             });
-            */
 
-            var sql = "INSERT INTO coins (date, tradeVol, tradeCount, marketCap, price, exchangeVol, genCoins, fees) VALUES ?";
-            con.query(sql, [date, tradeVol, tradeCount, marketCap, price, exchangeVol, genCoins, fees], function(err, results) {
+            var test = [
+                ['2013-05-01',108659660.293,52443,1542820000.0,139.0,0.0,3575.0,36.80599998],
+                ['2013-05-02',96958519.0041,55169,1292190000.0,116.38,0.0,3425.0,54.40791613],
+                ['2013-05-03',84459697.3245,55636,1180070000.0,106.25,0.0,3650.0,48.52677208]
+            ];*/
+            var sql = "INSERT INTO coins (coinName, date, tradeVol, tradeCount, marketCap, price, exchangeVol, genCoins, fees) VALUES ?";
+            con.query(sql, [fullCoinData], function(err, results) {
                 if (err) throw err;
                 console.log('inserted correctly');
             }); 
-          });
+        });
     });
 }
